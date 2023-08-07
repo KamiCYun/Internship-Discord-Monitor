114a67d9657358c08880cc74504559fba6ac9a4e
KamiCYun <KamiCYun@gmail.com>
2023-08-07 11:19:15 -0400

    changed status updates

--- src/bot.py
+++ src/bot.py
@@ -106,12 +106,12 @@ async def on_ready() -> None:
         await bot.tree.sync()
 
 
-@tasks.loop(minutes=1.0)
+@tasks.loop(minutes=10.0)
 async def status_task() -> None:
     """
     Setup the game status task of the bot.
     """
-    statuses = ["with you!", "with Krypton!", "with humans!"]
+    statuses = ["Peanuts!", "Noodles!", "Cats!"]
     await bot.change_presence(activity=discord.Game(random.choice(statuses)))
 
 
